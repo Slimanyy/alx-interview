0a80ab1f004f32a8d66ddf02b9b600beaca84fb8
Slimanyy <aabdulqudduss@gmail.com>
2023-07-27 04:01:01 -0700

    fixed pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,15 @@
+#!/usr/bin/python3
+
 def pascal_triangle(n):
+    """Returns characters representing Pascal's triangle."""
     if n <= 0:
         return []
 
     triangle = [[1]]
     for i in range(1, n):
         prev_row = triangle[-1]
-        next_row = [1] + [prev_row[j] + prev_row[j + 1] for j in range(len(prev_row) - 1)] + [1]
+        next_row = [1] + [prev_row[j] + prev_row[j + 1]
+                          for j in range(len(prev_row) - 1)] + [1]
         triangle.append(next_row)
 
     return triangle
-
