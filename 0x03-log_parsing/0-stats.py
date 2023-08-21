651171731b2cbabdef939c2bf697bf242c18db79
slimanyy <aabdulqudduss@gmail.com>
2023-08-21 21:01:01 +0100

    fixed executable file error

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-    script that reads stdin line by line and computes metrics
+    a script that reads stdin line by line and computes metrics
 """
 import sys
 
 
 if __name__ == '__main__':
-    # initialize variables to store metrics
+    # variables to store metrics
     total_file_size = 0
     codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     processed_line = 0
@@ -29,20 +29,20 @@ if __name__ == '__main__':
                 if status_code in statistics:
                     statistics[status_code] += 1
             except BaseException:
-                # if status code isn't in the correct position, skip line
+                # skip line if status code isn't in the correct position
                 pass
             try:
                 file_size = int(data[-1])
                 total_file_size += file_size
             except BaseException:
-                # if file size isn't in the correct position, skip line
+                # skip line if status code isn't in the correct position
                 pass
 
-            # After every 10 lines print statistics
+            #  print statistics After every 10 lines
             if processed_line % 10 == 0:
                 print_statistics(statistics, total_file_size)
         print_statistics(statistics, total_file_size)
     except KeyboardInterrupt:
-        # if the keyboard is interrupted (control C) print final statistics
+        #  print final statistics if the keyboard is interrupted (control C)
         print_statistics(statistics, total_file_size)
         raise
