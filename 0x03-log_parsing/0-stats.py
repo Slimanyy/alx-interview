8be859c043e4e25395b09e472c08a0f725e32bc3
slimanyy <aabdulqudduss@gmail.com>
2023-08-23 12:08:27 +0100

    updated log parser

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,39 @@
 #!/usr/bin/python3
 """
-    a script that reads stdin line by line and computes metrics
+Log parsing
 """
+
 import sys
 
-
 if __name__ == '__main__':
-    # variables to store metrics
-    total_file_size = 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    processed_line = 0
-    statistics = {}
-    for code in codes:
-        statistics[code] = 0
 
-    def print_statistics(statistics: dict, total_file_size: int):
-        print('File size: {}'.format(total_file_size))
-        for code, count in sorted(statistics.items()):
-            if count:
-                print("{}: {}".format(code, count))
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
+
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
     try:
         for line in sys.stdin:
-            processed_line += 1
+            count += 1
             data = line.split()
             try:
                 status_code = data[-2]
-                if status_code in statistics:
-                    statistics[status_code] += 1
+                if status_code in stats:
+                    stats[status_code] += 1
             except BaseException:
-                # skip line if status code isn't in the correct position
                 pass
             try:
-                file_size = int(data[-1])
-                total_file_size += file_size
+                filesize += int(data[-1])
             except BaseException:
-                # skip line if status code isn't in the correct position
                 pass
-
-            #  print statistics After every 10 lines
-            if processed_line % 10 == 0:
-                print_statistics(statistics, total_file_size)
-        print_statistics(statistics, total_file_size)
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
     except KeyboardInterrupt:
-        #  print final statistics if the keyboard is interrupted (control C)
-        print_statistics(statistics, total_file_size)
+        print_stats(stats, filesize)
         raise
